705865d62c939bd682871f31c5892b7b592e7298
myevertime <ilovelatale@naver.com>
2023-07-27 18:54:47 +0900

    added professor section

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from dash.dependencies import Input, Output, State
 from dash import dash_table
 import mysql.connector
 import pandas as pd
+from pymongo import MongoClient
 
 # Create the Dash app
 group_colors = {"control": "light blue", "reference": "red"}
@@ -22,9 +23,22 @@ db_connection = mysql.connector.connect(
     database='academicworld'
 )
 
+# Connect to local MongoDB
+client = MongoClient('localhost', 27017)
+mongodb = client['academicworld']
+
+# Fetch data from MongoDB and convert to DataFrame
+collection = mongodb['faculty']
+pipeline = [
+    {'$unwind': '$affiliation'},
+    {'$project': {'_id': 0, 'name': 1, 'position': 1, 'email' : 1,'phone' : 1, 'affiliation': '$affiliation.name', 'photoUrl' : 1}}
+]
+data_from_mongo = list(collection.aggregate(pipeline))
+mongodb_df = pd.DataFrame(data_from_mongo)
+
 ## Get the list of keywords by running keyword creation SQL file
 sql_lst = []
-with open('sql/createKeywordUnivView.sql', 'r') as f:
+with open('sql/createBaseTables.sql', 'r') as f:
     sql_statements = f.read().split(';')
     for statement in sql_statements:
         if statement != "":
@@ -55,7 +69,7 @@ app.layout = html.Div(
                 #html.P("")
             ],
         ),
-        # Body of the App
+        # First left body of the App
         html.Div(
             className="row app-body",
             children=[
@@ -69,11 +83,11 @@ app.layout = html.Div(
                                 html.Div(
                                     className="padding-top-bot",
                                     children=[
-                                        html.H6("Keyword"),
+                                        html.H6("Areas"),
                                         dcc.Dropdown(
                                             id='keyword-filter',
                                             options=[{'label': keyword, 'value': keyword} for keyword in keywords],
-                                            placeholder='Select an area that you\'re interested in',
+                                            placeholder='Select the area that you\'re interested in',
                                         ),
                                     ],
                                 ),
@@ -82,13 +96,13 @@ app.layout = html.Div(
                         html.Div(
                             className="bg-white2",
                             children=[
-                                html.H6("Top 5 keyword-matched universities"),
+                                html.H6("Top 5 matched universities"),
                                 dash_table.DataTable(
                                     id='keyword-univ-table',
                                     columns=[
                                         {'name': 'University', 'id': 'univ_name'},
-                                        {'name': 'Faculty Count', 'id': 'fac_cnt'},
-                                        {'name': 'Publications Count', 'id': 'pub_cnt'},
+                                        {'name': 'Faculty', 'id': 'fac_cnt'},
+                                        {'name': 'Publications', 'id': 'pub_cnt'},
                                         {'name': 'KRC', 'id': 'KRC'}
                                         # Add more columns for other numeric features as needed
                                     ],
@@ -96,22 +110,33 @@ app.layout = html.Div(
                                     style_table={'overflowX': 'auto'},
                                     style_cell={
                                         'height': 'auto',
+                                        'width': 'auto',
                                         'minWidth': '0px', 'maxWidth': '180px',
                                         'whiteSpace': 'normal'
-                                    }
+                                    },
+                                    style_header={
+                                        'fontWeight': 'bold',
+                                    },
+                                    style_data_conditional=[
+                                        {
+                                        'if': {'row_index': 0},
+                                        'backgroundColor': 'lightyellow',
+                                        'fontWeight': 'bold',
+                                        },
+                                    ],
                                 )
                             ],
                         ),
                         html.Div(
                             className="bg-white2",
                             children=[
-                                html.H6("Top 5 keyword-matched faculties"),
+                                html.H6("Top 5 matched faculties"),
                                 dash_table.DataTable(
-                                    id='keyword-univ-table2',
+                                    id='keyword-faculty-table',
                                     columns=[
                                         {'name': 'Faculty', 'id': 'fac_name'},
                                         {'name': 'University', 'id': 'univ_name'},
-                                        {'name': 'Publications Count', 'id': 'pub_cnt'},
+                                        {'name': 'Publications', 'id': 'pub_cnt'},
                                         {'name': 'KRC', 'id': 'KRC'}
                                         # Add more columns for other numeric features as needed
                                     ],
@@ -119,34 +144,117 @@ app.layout = html.Div(
                                     style_table={'overflowX': 'auto'},
                                     style_cell={
                                         'height': 'auto',
+                                        'width': 'auto',
                                         'minWidth': '0px', 'maxWidth': '180px',
                                         'whiteSpace': 'normal'
-                                    }
+                                    },
+                                    style_header={
+                                        'fontWeight': 'bold',
+                                    },
+                                    style_data_conditional=[
+                                        {
+                                        'if': {'row_index': 0},
+                                        'backgroundColor': 'lightyellow',
+                                        'fontWeight': 'bold',
+                                        },
+                                    ],
                                 )
                             ],
-                        )
+                        ),
+                        html.Div(
+                            className="bg-white3",
+                            children=[
+                                html.H6("Update New Area"),
+                                dcc.Input(
+                                        id='update-keyword',
+                                        type='text',
+                                        placeholder='Interested in other area?',
+                                        style = {'width':'300px', 'margin-right': '15px'} 
+                                ),
+                                html.Button('Update', id='update-button', n_clicks=0, style={'display':'right'}),
+                            ]
+                        ),
+                        # ConfirmDialog to show a pop-up when the update is successful
+                        dcc.ConfirmDialog(
+                            id='update-success-popup',
+                            message='Update successful! We will start to collect new data that you gave, so stay tuned! :)',
+                        ),
                     ],
                 ),
                 # Table
-                html.Div(
-                    className="eight columns card-left",
-                    children=[
-                        ##
-                    ],
-                ),
+                #html.Div(
+                #    className="eight columns card-left",
+                #    children=[
+                #        ##
+                #    ],
+                #),
                 #dcc.Store(id="error", storage_type="memory"),
             ],
         ),
+        # Second left body of the App
+        html.Div(
+            className="row app-body",
+            children=[
+                html.Div(
+                    className="five columns card",
+                    children=[
+                        html.Div(
+                            className="bg-white user-control",
+                            children=[
+                                html.Div(
+                                    className="padding-top-bot",
+                                    children=[
+                                        html.H6("Professor"),
+                                        # Dropdown to select the key for filtering
+                                        dcc.Dropdown(
+                                            id='filter-key',
+                                            options=[{'label': name, 'value': name} for name in list(mongodb_df['name'])],
+                                            value = 'Agouris,Peggy',
+                                            placeholder='Select the professor that you\'re interested in',
+                                        ),
+                                    ],
+                                ),
+                            ],
+                        ),
+                        html.Div(
+                            className="bg-white2",
+                            children=[
+                                # Bar chart to display filtered data
+                                html.Img(id="professor-img",src="", style={"width": "150px", "height": "200px", "border-radius": "80% / 50%", "margin-right": "10px"}),
+                                dcc.Textarea(
+                                    id="key-value-textarea",
+                                    value="",
+                                    style={"width": "100%", "height": "200px", "border":"None", "outline":"None"},
+                                    readOnly=True,
+                                ),
+                            ], style={"display": "flex"}
+                        ),
+                    ]
+                ),
+            ],
+        )
     ]
 )
 
 # Define the callback function to update the table based on the university filter input
 @app.callback(
-    Output('keyword-univ-table', 'data'),
-    [Input('keyword-filter', 'value')]
+    [Output('keyword-univ-table', 'data'),
+    Output('keyword-faculty-table', 'data'),
+    Output('update-keyword', 'value'),
+    Output('update-success-popup', 'displayed'),
+    Output("key-value-textarea", "value"),
+    Output("professor-img", "src")],
+    [Input('keyword-filter', 'value'),
+    Input('update-button', 'n_clicks'),
+    Input('filter-key', 'value')],
+    [State('update-keyword', 'value')]
 )
-def update_table(keyword):
-    # Fetch data from the MySQL database based on the selected university
+def update_table(keyword, n_clicks, filter_key, update_keyword_value):
+    ctx = dash.callback_context
+    trigger_id = ctx.triggered[0]['prop_id'].split('.')[0]
+    success_popup_displayed = False
+
+    ## First Table: keyword filter on university list
     cursor = db_connection.cursor()
     query = "SELECT * FROM keyword_univ_final"
     cursor.execute(query)
@@ -158,10 +266,47 @@ def update_table(keyword):
     filtered_df = df[df['keyword_name'] == keyword][['university_name', 'fac_cnt', 'pub_cnt', 'KRC']]
     filtered_df = filtered_df.sort_values('KRC', ascending=False)[:5]
 
-    # Prepare data for the table
-    table_data = [{'univ_name': row['university_name'], 'fac_cnt': row['fac_cnt'], 'pub_cnt':row['pub_cnt'], 'KRC':row['KRC']} for _, row in filtered_df.iterrows()]
-    return table_data
+    table_data1 = [{'univ_name': row['university_name'], 'fac_cnt': row['fac_cnt'], 'pub_cnt':row['pub_cnt'], 'KRC':row['KRC']} for _, row in filtered_df.iterrows()]
+    
+    ## Second Table: keyword filter on faculty list
+    cursor = db_connection.cursor()
+    query = "SELECT * FROM keyword_faculty_final"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    cursor.close()
 
+    df2 = pd.DataFrame(result)
+    df2.columns = ['fac_name', 'univ_name', 'keyword_name', 'pub_cnt', 'KRC']
+    filtered_df2 = df2[df2['keyword_name'] == keyword][['fac_name', 'univ_name', 'pub_cnt', 'KRC']]
+    filtered_df2 = filtered_df2.sort_values('KRC', ascending=False)[:5]
+
+    table_data2 = [{'fac_name': row['fac_name'], 'univ_name': row['univ_name'], 'pub_cnt':row['pub_cnt'], 'KRC':row['KRC']} for _, row in filtered_df2.iterrows()]
+    
+    # Check which input triggered the callback
+    if trigger_id == 'update-button':
+        # Update the database with the user keyword input from the web UI
+        cursor = db_connection.cursor()
+        query = "SELECT max(id) FROM keyword"
+        cursor.execute(query)
+        max_id = cursor.fetchall()
+        query = f"INSERT INTO keyword VALUES({max_id[0][0]+1},'{update_keyword_value}')"
+        cursor.execute(query)
+        db_connection.commit()
+        cursor.close()
+
+        success_popup_displayed = True
+    
+    # professor section
+    mongo_data = ""
+    photoUrl = ""
+    for json_ in data_from_mongo:
+        if json_['name'] == filter_key:
+            photoUrl = json_['photoUrl']
+            for key, value in json_.items():
+                if key != 'photoUrl' and value != "":
+                    mongo_data += f"{key.capitalize()} : {value}\n"
+
+    return table_data1, table_data2, '', success_popup_displayed, mongo_data, photoUrl
 
 # Run the app
 if __name__ == '__main__':
--- sql/createBaseTables.sql
+++ sql/createBaseTables.sql
@@ -3,6 +3,22 @@ CREATE TABLE IF NOT EXISTS keywordView AS (
     FROM keyword
     WHERE name in ('machine learning', 'artificial intelligence', 'computer vision', 'natural language processing')
 );
+CREATE TABLE IF NOT EXISTS facultyView AS (
+    SELECT faculty_id, keyword_id, score
+    FROM faculty_keyword
+    WHERE keyword_id in (
+        SELECT id
+        FROM keywordView
+    )
+);
+CREATE TABLE IF NOT EXISTS publicationView AS (
+    SELECT publication_id, keyword_id, score
+    FROM publication_keyword
+    WHERE keyword_id in (
+        SELECT id
+        FROM keywordView
+    )
+);
 CREATE TABLE IF NOT EXISTS univ_faculty AS (
     SELECT university_id, keyword_id, count(distinct id) as fac_cnt
     FROM (
@@ -10,14 +26,10 @@ CREATE TABLE IF NOT EXISTS univ_faculty AS (
         from faculty
     ) AS faculty
     INNER JOIN (
-        SELECT faculty_id, keyword_id
-        FROM faculty_keyword
-        WHERE keyword_id in (
-            SELECT id
-            FROM keywordView
-        )
-    ) as keywordview
-    ON faculty.id = keywordview.faculty_id
+        SELECT *
+        FROM facultyView
+    ) as facultyView
+    ON faculty.id = facultyView.faculty_id
     GROUP BY university_id, keyword_id
 );
 CREATE TABLE IF NOT EXISTS univ_publication AS (
@@ -33,13 +45,9 @@ CREATE TABLE IF NOT EXISTS univ_publication AS (
     ON faculty.id = faculty_pub.faculty_id
     INNER JOIN (
         SELECT *
-        FROM publication_keyword
-        WHERE keyword_id in (
-            SELECT id
-            FROM keywordView
-        )
-    ) as keywordview
-    ON faculty_pub.publication_id = keywordview.publication_id
+        FROM publicationView
+    ) as publicationView
+    ON faculty_pub.publication_id = publicationView.publication_id
     GROUP BY university_id, keyword_id
 );
 CREATE TABLE IF NOT EXISTS univ_krc AS (
@@ -57,12 +65,8 @@ CREATE TABLE IF NOT EXISTS univ_krc AS (
         ON t1.publication_id = t2.id
         INNER JOIN (
             SELECT *
-            FROM publication_keyword
-            WHERE keyword_id in (
-                SELECT id
-                FROM keywordView
-                )
-            ) as t3
+            FROM publicationView
+        ) as t3
         ON t1.publication_id = t3.publication_id
         GROUP BY faculty_id, keyword_id
     ) as t
@@ -99,4 +103,54 @@ CREATE TABLE IF NOT EXISTS keyword_univ_final AS (
         FROM keyword
     ) as meta2
     ON t1.keyword_id = meta2.id
+);
+CREATE TABLE IF NOT EXISTS univ_faculty_keyword AS (
+    SELECT keywordview.faculty_id, university_id, publicationView.keyword_id, count(publicationView.publication_id) as pub_cnt, sum(num_citations*score) as KRC
+    FROM (
+        SELECT faculty_id, keyword_id
+        FROM facultyView
+    ) AS keywordview
+    INNER JOIN (
+        SELECT *
+        FROM faculty_publication
+    ) as pub
+    ON keywordview.faculty_id = pub.faculty_id
+    INNER JOIN (
+        SELECT *
+        FROM publicationView
+    ) as publicationView
+    ON publicationView.publication_id = pub.publication_id
+    INNER JOIN (
+            SELECT id, num_citations
+            FROM publication
+    ) as publication
+    ON publication.id = publicationView.publication_id
+    INNER JOIN (
+        SELECT id, university_id
+        from faculty
+    ) as faculty
+    ON keywordview.faculty_id = faculty.id
+    GROUP BY faculty_id, university_id, keyword_id
+);
+CREATE TABLE IF NOT EXISTS keyword_faculty_final AS (
+    SELECT fac_name, univ_name, keyword_name, pub_cnt, KRC
+    FROM (
+        SELECT *
+        FROM univ_faculty_keyword
+    ) AS t1
+    INNER JOIN(
+        SELECT id, name as univ_name
+        FROM university
+    ) AS t2
+    ON t1.university_id = t2.id
+    INNER JOIN(
+        SELECT id, name as keyword_name
+        FROM keywordView
+    ) AS t3
+    ON t1.keyword_id = t3.id
+    INNER JOIN(
+        SELECT id, name as fac_name
+        FROM faculty
+    ) AS t4
+    ON t1.faculty_id = t4.id
 );
